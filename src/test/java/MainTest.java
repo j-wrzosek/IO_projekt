cd3deca3af2da4e96375797429739a6fd516e590
vequr <u56175@student.wat.edu.pl>
2025-05-29 13:22:08 +0200

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainTest {
 
     @Test
-    public void simpleTest() {
+    public void MainTest() {
         assertTrue(true);
     }
 }
